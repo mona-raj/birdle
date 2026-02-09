76eeeeec6da4b34f1bd40e4b3f0d0768d078cdb9
Mona Raj <agrmonaraj@gmail.com>
2026-02-09 10:02:31 +0530

    Create the anatomy of app and add game logic

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:birdle/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World! I am Mona'),
+        ),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace Container with widgets.
+    return Container();
+  }
+}
